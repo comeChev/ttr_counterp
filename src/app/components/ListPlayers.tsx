1a27250157c2af203f16a86f2827e9492d74ad21
Côme Chevallier <come.chevallier@icloud.com>
2023-05-24 14:47:05 +0200

    adjustments on modal ticket for mobile landscape mode

--- src/app/components/ListPlayers.tsx
+++ src/app/components/ListPlayers.tsx
@@ -16,7 +16,6 @@ import {
 } from "../library/atom/gameState";
 import { useEffect } from "react";
 import Link from "next/link";
-import useIsMobile from "../library/hooks/useIsMobile";
 import { BsPlayCircleFill } from "react-icons/bs";
 
 const PlayerIcon = ({
@@ -52,7 +51,6 @@ export default function ListPlayers() {
   const [game, setGame] = useRecoilState(gameState);
   const [currentPlayer, setCurrentPlayer] = useRecoilState(currentPlayerState);
   const [playersDefault, setPlayersDefault] = useRecoilState(playersState);
-  const isMobile = useIsMobile();
 
   function handleStorage() {
     if (window.localStorage.getItem("game")) {
@@ -67,58 +65,42 @@ export default function ListPlayers() {
     handleStorage();
   }, []);
   return (
-    <>
-      <div
-        className="
+    <div
+      className="
       flex flex-wrap 
       mt-5 
-      items-center 
-      w-full h-full 
-      sticky top-0 z-30
-      bg-white 
-      sm:mt-16 sm:flex-col sm:justify-evenly sm:sticky sm:top-0 sm:overflow-auto sm:self-start
+      items-center
+      justify-evenly
+      h-full w-full
+      sm:mt-16
+      sticky top-0 z-30 bg-white
       "
+    >
+      {game &&
+        game.players.map((player, index) => (
+          <PlayerIcon
+            key={index}
+            player={player}
+            handleClick={() => setCurrentPlayer(player)}
+          />
+        ))}
+
+      <Link
+        href="/"
+        className="group flex flex-col items-center justify-center space-y-1 sm:w-3 cursor-pointer group h-auto w-14 mx-1 my-1"
+        onClick={() => {
+          window.localStorage.removeItem("game");
+          setGame(defaultGame);
+          setPlayersDefault(defaultPlayers);
+        }}
       >
-        {game &&
-          game.players.map((player, index) => (
-            <PlayerIcon
-              key={index}
-              player={player}
-              handleClick={() => setCurrentPlayer(player)}
-            />
-          ))}
-        {!isMobile && (
-          <Link
-            href="/"
-            className="group flex flex-wrap sm:mt-5 sm:flex-col items-center sm:justify-center w-full text-center cursor-pointer hover:underline underline-offset-2"
-            onClick={() => {
-              window.localStorage.removeItem("game");
-              setGame(defaultGame);
-              setPlayersDefault(defaultPlayers);
-            }}
-          >
-            <div className="flex flex-col items-center justify-center">
-              <BsPlayCircleFill className="text-4xl group-hover:text-red-600 transition-colors duration-300" />
-              <p className={`text-sm text-gray-500 w-14 pt-1`}>
-                Nouvelle partie
-              </p>
-            </div>
-          </Link>
-        )}
-      </div>
-      {isMobile && (
-        <Link
-          href="/"
-          className="text-lg font-light text-blue-500 mt-5 w-full text-center cursor-pointer hover:underline underline-offset-2"
-          onClick={() => {
-            window.localStorage.removeItem("game");
-            setGame(defaultGame);
-            setPlayersDefault(defaultPlayers);
-          }}
-        >
-          Recommencer une partie
-        </Link>
-      )}
-    </>
+        <div className="flex flex-col items-center justify-center w-14">
+          <BsPlayCircleFill className="text-[45px] group-hover:text-red-600 transition-colors duration-300 border-2" />
+          <p className={`text-sm text-gray-500 w-14 pt-1 text-center`}>
+            Nouvelle partie
+          </p>
+        </div>
+      </Link>
+    </div>
   );
 }
--- src/app/components/ModalTicketPoints.tsx
+++ src/app/components/ModalTicketPoints.tsx
@@ -71,13 +71,13 @@ export default function ModalTicketPoints({
       initial={{ opacity: 0, top: -20 }}
       animate={{ opacity: 1, top: 10 }}
       exit={{ opacity: 0, top: -20 }}
-      className={`fixed  top-4 ${backgroundExtraLight} text-neutral-700 border-2 ${borderColor} p-4 rounded-lg w-[85%] left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center px-10 z-50`}
+      className={`fixed top-4 flex flex-col justify-evenly items-center ${backgroundExtraLight} text-neutral-700 border-2 ${borderColor} p-2 rounded-lg w-[85%] h-[60%] left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center px-10 z-50 overflow-scroll`}
     >
-      <div className="flex flex-wrap w-full sm:w-3/4">
+      <div className="flex flex-wrap w-full pt-4">
         {routesPoints.map((routePoint, index) => (
           <button
             key={index}
-            className="bg-white border-2 border-gray-400 m-2 focus:bg-gray-500 focus:text-white w-[60px] h-[40px] sm:w-[120px] sm:h-[60px] rounded-md"
+            className="bg-white border-2 border-gray-400 m-2 focus:bg-gray-500 focus:text-white w-[60px] h-[40px] sm:w-[70px] sm:h-[35px] rounded-md"
             onClick={() => setValue(routePoint)}
           >
             {routePoint}
@@ -85,7 +85,7 @@ export default function ModalTicketPoints({
         ))}
       </div>
 
-      <div className="grid grid-cols-2 gap-5 h-12 w-full sm:w-3/4 mt-10 mb-5">
+      <div className="grid grid-cols-2 gap-5 h-6 w-full sm:w-3/4 mt-3 pb-5">
         <button
           disabled={value === 0}
           onClick={() => addTicketPoint()}
@@ -93,7 +93,7 @@ export default function ModalTicketPoints({
             value === 0 ? "text-green-500" : "text-white"
           }`}
         >
-          {value === 0 ? "0" : `Ajouter ${value} points`}
+          {value === 0 ? "Ajouter 0 points" : `Ajouter ${value} points`}
         </button>
         <button
           disabled={value === 0}
@@ -102,12 +102,11 @@ export default function ModalTicketPoints({
             value === 0 ? "text-red-500" : "text-white"
           }`}
         >
-          {value === 0 ? "0" : `Retirer ${value} points`}
+          {value === 0 ? "Retirer 0 points" : `Retirer ${value} points`}
         </button>
       </div>
-
       <span
-        className="cursor-pointer text-lg text-red-500 hover:underline underline-offset-2"
+        className="absolute top-1 cursor-pointer text-lg text-blue-500 hover:underline underline-offset-2 w-full text-center col-span-2"
         onClick={() => setIsModalOpen(false)}
       >
         Fermer
